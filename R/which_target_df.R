6510d01969c0cbccac5074fb2999942d0e092891
donco <colin.donald@outlook.com>
2020-05-20 10:31:04 -0700

    Validation check for assessment

--- R/which_target_df.R
+++ R/which_target_df.R
@@ -12,21 +12,23 @@ which_target_df <- function(df, all_obs = TRUE){
   df <- merge(df, tmdl_db[, c("ReachCode", "pollutant_name_AWQMS", "geo_id", "target_value", "target_units", "target_stat_base", "season_start", "season_end")],
               by.x = c("Reachcode", "Char_Name"), by.y = c("ReachCode", "pollutant_name_AWQMS"), all.x = all_obs, all.y = FALSE)
 
-  df <- df %>% dplyr::mutate(
-    # Append start and end dates with year
-    start_datetime = ifelse(!is.na(season_start), paste0(season_start, "-", lubridate::year(sample_datetime)), NA ) ,
-    end_datetime = ifelse(!is.na(season_end), paste0(season_end, "-", lubridate::year(sample_datetime)), NA ),
-    # Make dates POSIXct format
-    start_datetime = as.POSIXct(start_datetime, format = "%d-%b-%Y"),
-    end_datetime = as.POSIXct(end_datetime, format = "%d-%b-%Y"),
-    # If dates span a calendar year, account for year change in end date
-    end_datetime = if_else(end_datetime < start_datetime & date >= end_datetime, end_datetime + lubridate::years(1), # add a year if inperiod carrying to next year
-                           end_datetime), # otherwise, keep End_spawn as current year
-    start_datetime = if_else(end_datetime < start_datetime & date <= end_datetime, start_datetime - lubridate::years(1), # subtract a year if in period carrying from previous year
-                             start_datetime),
-    tmdl_season = if_else(sample_datetime >= start_datetime & sample_datetime <= end_datetime, TRUE, FALSE),
-    criteria = if_else(tmdl_season, geo_id, NA_character_)
+  if(nrow(df) > 0){
+    df <- df %>% dplyr::mutate(
+      # Append start and end dates with year
+      start_datetime = ifelse(!is.na(season_start), paste0(season_start, "-", lubridate::year(sample_datetime)), NA ) ,
+      end_datetime = ifelse(!is.na(season_end), paste0(season_end, "-", lubridate::year(sample_datetime)), NA ),
+      # Make dates POSIXct format
+      start_datetime = as.POSIXct(start_datetime, format = "%d-%b-%Y"),
+      end_datetime = as.POSIXct(end_datetime, format = "%d-%b-%Y"),
+      # If dates span a calendar year, account for year change in end date
+      end_datetime = if_else(end_datetime < start_datetime & date >= end_datetime, end_datetime + lubridate::years(1), # add a year if inperiod carrying to next year
+                             end_datetime), # otherwise, keep End_spawn as current year
+      start_datetime = if_else(end_datetime < start_datetime & date <= end_datetime, start_datetime - lubridate::years(1), # subtract a year if in period carrying from previous year
+                               start_datetime),
+      tmdl_season = if_else(sample_datetime >= start_datetime & sample_datetime <= end_datetime, TRUE, FALSE),
+      criteria = if_else(tmdl_season, geo_id, NA_character_)
     ) %>% dplyr::select(-season_start, -season_end, -geo_id)
+  }
 
   return(df)
 

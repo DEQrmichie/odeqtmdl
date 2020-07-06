75cc853bc132e44f8f76a20d431c5fb6a01f8585
DEQrmichie <michie.ryan@deq.state.or.us>
2020-07-06 15:01:19 -0700

    Added tmdl_db function
    Formal function to pull database.

---
+++ R/tmdl_db.R
@@ -0,0 +1,13 @@
+#' Oregon TMDL database
+#'
+#'
+#' @docType data
+#' @usage data(tmdl_db)
+#' @keywords Oregon TMDL database
+#' @keywords datasets
+#' @examples
+#' db1 <- data(tmdl_db)
+#' db2 <-odeqtmdl::tmdl_db
+#'
+
+"tmdl_db"

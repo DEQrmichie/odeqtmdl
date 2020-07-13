7bf23e168fca20ee1ec7089d9b6fa5763a56a0bb
donco <colin.donald@outlook.com>
2020-07-13 16:08:44 -0700

    Added tmdl info for mapping popup

--- R/which_target_df.R
+++ R/which_target_df.R
@@ -9,13 +9,16 @@
 
 which_target_df <- function(df, all_obs = TRUE){
 
-  tmdl_db_mins <- odeqtmdl::tmdl_db[, c("ReachCode", "pollutant_name_AWQMS", "geo_id", "TMDL_name",
+  tmdl_db_mins <- odeqtmdl::tmdl_db[, c("ReachCode", "pollutant_name_AWQMS", "geo_id", "TMDL_name", "approval_year",
                                         "value_conv", "units_conv", "target_stat_base", "target_type",
                                         "season_start", "season_end", "target_conditionals_references")] %>%
     dplyr::filter(is.na(target_conditionals_references), target_type %in% c("temperature", "concentration")) %>%
-    dplyr::group_by(ReachCode, pollutant_name_AWQMS, units_conv, target_stat_base, TMDL_name,
+    dplyr::group_by(ReachCode, pollutant_name_AWQMS, units_conv, target_stat_base, TMDL_name, approval_year,
                     season_start, season_end) %>%
-    dplyr::summarise(target_value = min(value_conv, na.rm = TRUE))
+    dplyr::summarise(target_value = min(value_conv, na.rm = TRUE)) %>%
+    dplyr::mutate(tmdl_period = paste(season_start, "-", season_end),
+                  tmdl = paste0(TMDL_name, " (DEQ ", approval_year, ")")) %>%
+    dplyr::select(-approval_year, -TMDL_name)
 
   df <- merge(df, tmdl_db_mins,
               by.x = c("Reachcode", "Char_Name"), by.y = c("ReachCode", "pollutant_name_AWQMS"), all.x = all_obs, all.y = FALSE)

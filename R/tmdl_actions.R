c231b004ab1cbe81a5a9610c6fbc8c77188f07c1
DEQrmichie <michie.ryan@deq.state.or.us>
2020-07-27 23:30:47 -0700

    documentation update

--- R/tmdl_actions.R
+++ R/tmdl_actions.R
@@ -5,7 +5,7 @@
 #' \itemize{
 #'   \item action_id:	USEPA ATTAINS Action ID assigned to each TMDL document.
 #'   \item TMDL_name:	Name of TMDL document.
-#'   \item TMDL_issue_year:	Year the TMDL was issued by Oregon DEQ.
+#'   \item TMDL_issue_year:	Year the TMDL was issued by the issue agency..
 #'   \item TMDL_active:	Boolean to indicate if the TMDL and TMDL allocations are effective and being implemented.
 #'   \item issue_agency:	The agency that developed and issued the TMDL (OR or EPA).
 #'   \item in_attains:	Boolean to indicate if the TMDL action has been entered into EPA's ATTAINS database.

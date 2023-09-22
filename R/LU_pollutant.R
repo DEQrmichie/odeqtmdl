7dcc82138066f546edc133cbae4dfb23516d8131
DEQrmichie <ryan.michie@deq.oregon.gov>
2023-09-21 17:51:17 -0700

    Adding LU_pollutant table

---
+++ R/LU_pollutant.R
@@ -0,0 +1,32 @@
+#' Pollutant parameter look up
+#'
+#' Lookup table of DEQ and EPA ATTAINS water quality parameter names and IDs.
+#'
+#' Data fields include:
+#' \itemize{
+#'   \item Pollu_ID: DEQ water quality parameter ID.
+#'   \item Pollutant_DEQ: DEQ water quality parameter name.
+#'   \item CAS: Chemical Abstracts Service registry number.
+#'   \item WQS_Unit: Unit used for the parameter in the water quality standards.
+#'   \item Unit_UID: Unit ID.
+#'   \item Attains_Pollutant: EPA ATTAINS water quality parameter name.
+#'   \item Attains_Group: EPA ATTAINS water quality parameter group.
+#' }
+#'
+#' @docType data
+#' @usage data(LU_pollutant)
+#' @keywords Oregon TMDL database
+#' @keywords datasets
+#' @examples
+#' polluLU1 <- data(LU_pollutant)
+#' polluLU2 <- odeqtmdl::LU_pollutant
+#'
+
+"LU_pollutant"
+
+
+
+
+
+
+
